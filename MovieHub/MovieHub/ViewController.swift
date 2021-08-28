84a3454acda8175177e7055e3c421ea14a46a67a
Van Linh Nguyen <linhvnguyen9@gmail.com>
2021-08-28 12:09:24 +0700

    Refs[TASK][40972] Init project

---
+++ MovieHub/MovieHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieHub
+//
+//  Created by Van Linh Nguyen on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
